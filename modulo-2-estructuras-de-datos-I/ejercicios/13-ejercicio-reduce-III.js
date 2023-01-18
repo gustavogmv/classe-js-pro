0bd4b4c769663bdafc54012b5d42627afb0994ce
ggsp <gusanchez@gmv.com>
2023-01-18 17:00:36 +0100

    classe / js / day03

--- modulo-2-estructuras-de-datos-I/ejercicios/13-ejercicio-reduce-III.js
+++ modulo-2-estructuras-de-datos-I/ejercicios/13-ejercicio-reduce-III.js
@@ -3,7 +3,7 @@
  */
 
 function sumMatrix(matrix) {
-  // ?
+    return matrix.map(row => row.reduce((acc, n) => acc + n, 0))
 }
 
 console.log(sumMatrix([[1, 2], [34, 20, 5], [11], [2, 4]])); // [3, 59, 11, 6]
