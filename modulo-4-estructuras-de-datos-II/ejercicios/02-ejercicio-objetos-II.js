8214d377b41230bb75caa69c208866464fc6eeb9
ggsp <gusanchez@gmv.com>
2023-01-23 17:25:17 +0100

    classe / js / [week 2] / day05

--- modulo-4-estructuras-de-datos-II/ejercicios/02-ejercicio-objetos-II.js
+++ modulo-4-estructuras-de-datos-II/ejercicios/02-ejercicio-objetos-II.js
@@ -3,4 +3,12 @@
  * los valores de un objeto.
  */
 
-console.log(mapValues({ a: 1, b: 2, c: 3 }, (x) => x * 2)); // { a: 2, b: 4, c: 6 }
+function mapValues(object, transformer) {
+    return Object.keys(object).reduce((acc, key) => {
+        const transformedValue = transformer(object[key])
+        acc[key] = transformedValue
+        return acc
+    }, {})
+}
+
+console.log(mapValues({a: 1, b: 2, c: 3}, (x) => x * 2)); // { a: 2, b: 4, c: 6 }
--- modulo-4-estructuras-de-datos-II/ejercicios/05-ejercicio-objetos-V.js
+++ modulo-4-estructuras-de-datos-II/ejercicios/05-ejercicio-objetos-V.js
@@ -3,10 +3,15 @@
  * El resultado debería ser 23.
  */
 
-const obj = { a: 1, b: { c: 2, d: 5, e: { f: 9, g: 6 } } };
+const obj = {a: 1, b: {c: 2, d: 5, e: {f: 9, g: 6}}};
 
 function sumObject(obj) {
-  // ?
+    return Object.keys(obj).reduce((acc, key) => {
+        if (typeof obj[key] !== "object") {
+            return acc + obj[key]
+        } else return acc + sumObject(obj[key])
+    }, 0)
 }
 
-console.log(sumObject({ a: 1, b: { c: 2, d: 5, e: { f: 9, g: 6 } } })); // 23
+console.log(sumObject({a: 1, b: {c: 2, d: 5, e: {f: 9, g: 6}}})); // 23
+console.log(sumObject(obj)); // 23
