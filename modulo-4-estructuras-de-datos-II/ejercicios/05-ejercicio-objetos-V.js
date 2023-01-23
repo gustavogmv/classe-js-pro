f16ef0fd41d16bc23d4c27ccae2891bfe12884a9
ggsp <gusanchez@gmv.com>
2023-01-23 17:26:19 +0100

    classe / js / [week 2] / day05

--- modulo-4-estructuras-de-datos-II/ejercicios/05-ejercicio-objetos-V.js
+++ modulo-4-estructuras-de-datos-II/ejercicios/05-ejercicio-objetos-V.js
@@ -6,11 +6,14 @@
 const obj = {a: 1, b: {c: 2, d: 5, e: {f: 9, g: 6}}};
 
 function sumObject(obj) {
-    return Object.keys(obj).reduce((acc, key) => {
-        if (typeof obj[key] !== "object") {
-            return acc + obj[key]
-        } else return acc + sumObject(obj[key])
-    }, 0)
+    // return Object.keys(obj).reduce((acc, key) => {
+    //     if (typeof obj[key] !== "object") {
+    //         return acc + obj[key]
+    //     } else return acc + sumObject(obj[key])
+    // }, 0)
+    return Object.values(obj).reduce(
+        (acc, value) => (acc += typeof value === "object" ? sumObject(value) : value), 0
+    )
 }
 
 console.log(sumObject({a: 1, b: {c: 2, d: 5, e: {f: 9, g: 6}}})); // 23
