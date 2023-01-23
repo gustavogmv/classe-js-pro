7439856414854316af618e0108ebfdb09889d391
ggsp <gusanchez@gmv.com>
2023-01-23 18:33:12 +0100

    classe / js / [week 2] / day05

--- modulo-4-estructuras-de-datos-II/ejercicios/09-ejercicio-destructuring-II.js
+++ modulo-4-estructuras-de-datos-II/ejercicios/09-ejercicio-destructuring-II.js
@@ -2,12 +2,14 @@
  * Desestructura el siguiente objeto en las variables uno, dos, tres, cuatro y cinco.
  */
 
-const obj = { uno: 1, lista: [2, 3], cuatro: 4, x: { cinco: 5 } };
+const obj = {uno: 1, lista: [2, 3], cuatro: 4, x: {cinco: 5}};
 
-// ?
+const {uno, lista: [dos, tres], cuatro, x: {cinco}} = obj
+const {x: {cinco: otrocinco}} = obj
 
 console.log(uno); // 1
 console.log(dos); // 2
 console.log(tres); // 3
 console.log(cuatro); // 4
 console.log(cinco); // 5
+console.log(otrocinco); // 5
