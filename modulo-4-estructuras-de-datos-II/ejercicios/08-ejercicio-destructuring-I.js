e5ee15a85d939ab16c675fb45a20364df1545791
ggsp <gusanchez@gmv.com>
2023-01-23 18:24:44 +0100

    classe / js / [week 2] / day05

--- modulo-4-estructuras-de-datos-II/ejercicios/08-ejercicio-destructuring-I.js
+++ modulo-4-estructuras-de-datos-II/ejercicios/08-ejercicio-destructuring-I.js
@@ -2,16 +2,16 @@
  * Desestructura el objeto { uno: 1, dos: 2 } en dos variables: uno y dos.
  */
 
-const obj = {uno: 1, dos: 2};
+const obj = {uno_: 1, dos_: 2};
 {
-    const {dos, uno} = obj
+    const {dos_, uno_} = obj
 
-    console.log(uno); // 1
-    console.log(dos); // 2
+    console.log(uno_); // 1
+    console.log(dos_); // 2
 }
 
 {
-    const {dos: uno, uno: dos} = obj
+    const {dos_: uno, uno_: dos} = obj
 
     console.log(uno); // 2
     console.log(dos); // 1
