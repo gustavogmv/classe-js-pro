f84790e1ad0f78ffd8c84dce15fdf041cea8fce5
ggsp <gusanchez@gmv.com>
2023-01-24 17:43:52 +0100

    classe / js / [week 2] / day06

--- modulo-4-estructuras-de-datos-II/ejercicios/13-ejercicio-getter-setters.js
+++ modulo-4-estructuras-de-datos-II/ejercicios/13-ejercicio-getter-setters.js
@@ -4,6 +4,12 @@
 
 const array = [1, 2, 3];
 
-// ?
+Object.defineProperty(array, 'average', {
+    get: () => {
+        return array.reduce((acc, e) => acc + e / array.length, 0)
+    }
+})
 
 console.log(array.average); // Deberiamos obtener 2
+array.push(4, 5)
+console.log(array.average); // Deberiamos obtener 3
