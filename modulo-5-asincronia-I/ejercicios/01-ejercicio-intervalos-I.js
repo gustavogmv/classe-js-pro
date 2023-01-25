46abd7ab9fe7cd72dc22a5ff716831da2f6dda70
ggsp <gusanchez@gmv.com>
2023-01-25 17:10:12 +0100

    classe / js / [week 2] / day07

--- modulo-5-asincronia-I/ejercicios/01-ejercicio-intervalos-I.js
+++ modulo-5-asincronia-I/ejercicios/01-ejercicio-intervalos-I.js
@@ -3,4 +3,18 @@
  * Incrementa el contador cada segundo e imprímelo.
  */
 
-setTimeout(()=> console.log("Timeout..."),1000)
+let contador = 0
+let counter2 = 0
+const incAndLog = (maxCount) => setTimeout(() => {
+    console.log(++contador)
+    if (maxCount > 1 && contador < maxCount) {
+        incAndLog(maxCount)
+    }
+}, 1000)
+incAndLog(60)
+const counter2Interval = setInterval(() => {
+    console.log("Counter2: ", ++counter2)
+    if (counter2 > 60) {
+        clearInterval(counter2Interval)
+    }
+}, 999)
