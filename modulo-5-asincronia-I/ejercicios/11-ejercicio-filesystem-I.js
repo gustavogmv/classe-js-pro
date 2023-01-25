4fce36187ff2ec87e06724d2612dd1d3664f7273
ggsp <gusanchez@gmv.com>
2023-01-25 18:55:47 +0100

    classe / js / [week 2] / day07

--- modulo-5-asincronia-I/ejercicios/11-ejercicio-filesystem-I.js
+++ modulo-5-asincronia-I/ejercicios/11-ejercicio-filesystem-I.js
@@ -5,8 +5,9 @@ const path = require("path")
 function calculateDirSize(dir, callback) {
     let dirSize = 0
     fs.readdir(dir, (error, result) => {
-        if(error) throw error
+        if (error) throw error
         const files = result.map((file) => path.resolve(dir, file))
+
         fs.stat(files[0], (error, result) => {
             dirSize += result.size
             fs.stat(files[1], (error, result) => {
