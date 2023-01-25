0347d5990de27949b89b1a77849850f6a82c83cb
ggsp <gusanchez@gmv.com>
2023-01-25 17:15:43 +0100

    classe / js / [week 2] / day07

--- modulo-5-asincronia-I/ejercicios/02-ejercicio-intervalos-II.js
+++ modulo-5-asincronia-I/ejercicios/02-ejercicio-intervalos-II.js
@@ -3,4 +3,10 @@
  * No utilices setInterval
  */
 
-// ?
+const ping = (maxCount, period, count=0) => setTimeout(() => {
+    console.log(Date.now(),"ping [",count,"]")
+    if (maxCount > 1 && count < maxCount) {
+        ping(maxCount, period, ++count)
+    }
+}, 500)
+ping(60, 500)
