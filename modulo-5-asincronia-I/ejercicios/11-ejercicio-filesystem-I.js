ce90b9248c28a12389e72d970c370feaa4804520
ggsp <gusanchez@gmv.com>
2023-01-26 16:44:27 +0100

    classe / js / [week 2] / day08

--- modulo-5-asincronia-I/ejercicios/11-ejercicio-filesystem-I.js
+++ modulo-5-asincronia-I/ejercicios/11-ejercicio-filesystem-I.js
@@ -4,18 +4,32 @@ const path = require("path")
 
 function calculateDirSize(dir, callback) {
     let dirSize = 0
-    fs.readdir(dir, (error, result) => {
+    let filesCount = 0
+    let filesReaded = 0
+    fs.readdir(dir, (error, fileList) => {
         if (error) throw error
-        const files = result.map((file) => path.resolve(dir, file))
+        const files = fileList.map((file) => path.resolve(dir, file))
+        console.log(files)
 
-        fs.stat(files[0], (error, result) => {
-            dirSize += result.size
-            fs.stat(files[1], (error, result) => {
-                dirSize += result.size
-                callback(dirSize)
+        filesCount = files.length
+
+        files.forEach((file, index) => {
+            console.log("Empezamos operacion asincrona", file, index)
+
+            fs.stat(file, (error, fstats) => {
+                if (error) throw error
+
+                filesReaded++
+                dirSize += fstats.size
+
+                if(filesReaded === filesCount){
+                    callback(dirSize)
+                }
+
+                console.log(fstats.size)
             })
         })
     })
 }
 
-calculateDirSize("../../ejercicio-final", (size) => console.log(size))
+calculateDirSize("../../ejercicio-final", (size) => console.log("TOTAL SIZE: ", size))
