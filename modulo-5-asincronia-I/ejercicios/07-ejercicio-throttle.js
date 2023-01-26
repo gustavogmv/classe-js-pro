1b1717797cee633cc4a9f7356e9d60a81efa7e79
ggsp <gusanchez@gmv.com>
2023-01-26 17:23:59 +0100

    classe / js / [week 2] / day08

--- modulo-5-asincronia-I/ejercicios/07-ejercicio-throttle.js
+++ modulo-5-asincronia-I/ejercicios/07-ejercicio-throttle.js
@@ -12,13 +12,13 @@ function throttle(func, milliseconds) {
     }
 }
 
-const spam = (msg) => console.log("SPAM!", msg);
+const spam = (msg) => console.log(Date.now(), "SPAM!", msg);
 
 const throttledSpam = throttle(spam, 500);
 
 throttledSpam("haha"); // SPAM!
-throttledSpam(); // (Sin log)
-throttledSpam(); // (Sin log)
-throttledSpam(); // (Sin log)
+throttledSpam("hoho"); // (Sin log)
+throttledSpam("hehe"); // (Sin log)
+throttledSpam("hihi"); // (Sin log)
 
-setTimeout(throttledSpam, 600); // SPAM!
+setTimeout(() => {throttledSpam("llll")}, 600); // SPAM!
