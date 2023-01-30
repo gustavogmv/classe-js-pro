2c99f5e9d94c5cc59ad32125dcdb7cbcf77340c6
ggsp <gusanchez@gmv.com>
2023-01-30 18:06:03 +0100

    classe / js / [week 3] / day09

--- modulo-6-asincronia-II/ejercicios/06-ejercicio-promesas-VI.js
+++ modulo-6-asincronia-II/ejercicios/06-ejercicio-promesas-VI.js
@@ -5,13 +5,18 @@
 const throwDice = require("./03-ejercicio-promesas-III");
 
 function getPlayerScore() {
-  // ?
+    return Promise.all([
+        throwDice(),
+        throwDice()]).then(scores => scores)
 }
 
 function startGame() {
-  // ?
+    return Promise.all([
+        getPlayerScore(),
+        getPlayerScore(),
+        getPlayerScore()]).then(values => values)
 }
 
 startGame().then((result) => {
-  console.log(result); // [[2, 2], [4, 6], [5, 1]]
+    console.log(result); // [[2, 2], [4, 6], [5, 1]]
 });
