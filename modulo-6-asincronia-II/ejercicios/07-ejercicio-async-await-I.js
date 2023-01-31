8777ce36dbfa7b4e222e25cda15dcd2a713b4ccc
ggsp <gusanchez@gmv.com>
2023-01-31 17:28:40 +0100

    classe / js / [week 3] / day10 -- parallelize: use Promise.all

--- modulo-6-asincronia-II/ejercicios/07-ejercicio-async-await-I.js
+++ modulo-6-asincronia-II/ejercicios/07-ejercicio-async-await-I.js
@@ -10,24 +10,25 @@ const wait = require("./02-ejercicio-promesas-II");
 
 
 async function throwDice() {
-    await wait(599);
+    await wait(1000);
     return Math.ceil(Math.random() * 6)
 }
 
 async function getPlayerScore() {
-    const r1 = await throwDice();
-    const r2 = await throwDice();
-    return [r1, r2]
+    // const r1 = await throwDice();
+    // const r2 = await throwDice();
+    // return [r1, r2]
+    await Promise.all([throwDice(), throwDice()])
 }
 
+
+async function startGame() {
+    // const player = [await getPlayerScore(), await getPlayerScore(), await getPlayerScore()]
+    await Promise.all([getPlayerScore(), getPlayerScore(), getPlayerScore()])
+}
 
 console.time("Start.Game....")
-async function startGame() {
-    const player =  [await getPlayerScore(), await getPlayerScore(), await getPlayerScore()]
-    console.timeEnd("Start.Game....")
-    return player
-}
-
 startGame().then((result) => {
     console.log(result); // [[2, 2], [4, 6], [5, 1]]
+    console.timeEnd("Start.Game....")
 });
