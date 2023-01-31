619a833d9a3ca038d0afb4fe96fde6f8583d2f09
ggsp <gusanchez@gmv.com>
2023-01-31 16:54:23 +0100

    classe / js / [week 3] / day10

--- modulo-6-asincronia-II/ejercicios/07-ejercicio-async-await-I.js
+++ modulo-6-asincronia-II/ejercicios/07-ejercicio-async-await-I.js
@@ -8,9 +8,6 @@
 
 const wait = require("./02-ejercicio-promesas-II");
 
-// return Wait(601).then(() => {
-//   return Math.ceil(Math.random() * 6)
-// })
 
 async function throwDice() {
     await wait(599);
@@ -18,14 +15,19 @@ async function throwDice() {
 }
 
 async function getPlayerScore() {
-    // ?
+    const r1 = await throwDice();
+    const r2 = await throwDice();
+    return [r1, r2]
 }
 
+
+console.time("Start.Game....")
 async function startGame() {
-    // ?
+    const player =  [await getPlayerScore(), await getPlayerScore(), await getPlayerScore()]
+    console.timeEnd("Start.Game....")
+    return player
 }
 
-throwDice().then(console.log)
 startGame().then((result) => {
     console.log(result); // [[2, 2], [4, 6], [5, 1]]
 });
