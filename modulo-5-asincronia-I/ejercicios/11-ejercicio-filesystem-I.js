687ef824a61a1dbc006aac14633fdd3ad9ecb457
ggsp <gusanchez@gmv.com>
2023-01-31 17:47:10 +0100

    classe / js / [week 3] / day10 --

--- modulo-5-asincronia-I/ejercicios/11-ejercicio-filesystem-I.js
+++ modulo-5-asincronia-I/ejercicios/11-ejercicio-filesystem-I.js
@@ -26,4 +26,26 @@ function calculateDirSize(dir, callback) {
     })
 }
 
+function calculateDirSizeAsync(dir, callback) {
+
+    fs.readdir(dir, (error, fileList) => {
+        if (error) throw error
+        const files = fileList.map((file) => path.resolve(dir, file))
+
+        async function mapFileToSize(file) {
+            const stats = await fs.promises.stat(file)
+            return stats.size
+        }
+
+        function onListMapped(list) {
+            console.log("Array Mapped==-=> ", list)
+            const sum = list.reduce((total, n) => total + n, 0);
+            callback(sum);
+        }
+
+        asyncMap(files, mapFileToSize, onListMapped)
+    })
+}
+
 calculateDirSize("../../ejercicio-final", (size) => console.log("TOTAL SIZE: ", size))
+calculateDirSizeAsync("../../ejercicio-final", (size) => console.log("TOTAL SIZE ASYNC: ", size))
