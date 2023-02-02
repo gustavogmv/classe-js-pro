de6af3a68dc18cb7f762d4bad4a23c90f9951f6b
ggsp <gusanchez@gmv.com>
2023-02-02 18:48:56 +0100

    classe / js / [week 3] / day12 ...
    Number generator ------

--- symbol-generators/ejercicios/07-ejercicio-generators-VI.js
+++ symbol-generators/ejercicios/07-ejercicio-generators-VI.js
@@ -6,29 +6,28 @@
 
 const futureValue = (value) => new Promise((resolve) => setTimeout(() => resolve(value || "missing value"), 1000));
 
-async function* generator() {
-    let val = Promise.resolve(0)
+function* generator() {
+    let val
     while (true) {
-        const valor = await val.then(v => v).catch(console.log)
-        val = yield valor
+        val = yield val
     }
 }
 
-function main() {
+async function main() {
     const gen = generator();
 
     gen.next();
 
-    const first = gen.next(futureValue(1)).value;
+    const first = gen.next(await futureValue(1)).value;
     console.log(first); // 1
 
-    const second = gen.next(futureValue(2)).value;
+    const second = gen.next(await futureValue(2)).value;
     console.log(second); // 2
 
-    const third = gen.next(futureValue(3)).value;
+    const third = gen.next(await futureValue(3)).value;
     console.log(third); // 3
 
-    const fourth = gen.next(futureValue(4)).value;
+    const fourth = gen.next(await futureValue(4)).value;
     console.log(fourth); // 4
 }
 
